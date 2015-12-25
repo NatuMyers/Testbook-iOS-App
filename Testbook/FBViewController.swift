2a81ccbd196314a2d21ecbac95d787b7752e9ea6
Natu Myers <me@natumyers.com>
2015-12-24 20:40:08 -0500

    init

---
+++ Testbook/FBViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FBViewController.swift
+//  Testbook
+//
+//  Created by Natu Myers on 2015-12-24.
+//  Copyright © 2015 Raptroop. All rights reserved.
+//
+
+import UIKit
+
+class FBViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
